52a5a7980c6b45cc93b6b2c65973f22fa450c4e7
ravis <ravissavoy49@gmail.com>
2022-01-03 10:41:56 -0600

    Created a base for ui text

---
+++ raygame/UIText.cpp
@@ -0,0 +1,13 @@
+#include "UIText.h"
+
+UIText::UIText(const char* text, int width, int height, int fontSize, Texture2D* font, Texture2D* fontColor)
+{
+}
+
+void UIText::start()
+{
+}
+
+void UIText::draw()
+{
+}
--- raygame/UIText.h
+++ raygame/UIText.h
@@ -1,6 +1,17 @@
 #pragma once
-class UIText
+#include "Actor.h"
+
+class Texture2D;
+
+class UIText :
+	public Actor
 {
+public:
+	UIText(const char* text, int width, int height, int fontSize, Texture2D* font, Texture2D* fontColor);
+
+	void start() override;
+
+	void draw() override;
 
 };
 
