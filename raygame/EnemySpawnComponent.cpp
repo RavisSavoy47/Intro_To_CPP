b8bafb1345c9d1746c55e319ab9ea8f8b0c36399
Will <montero_w@live.com>
2022-01-05 14:17:09 -0600

    committing last comments

--- raygame/EnemySpawnComponent.cpp
+++ raygame/EnemySpawnComponent.cpp
@@ -7,7 +7,7 @@ void EnemySpawnComponent::start()
 {
 	Component::start();
 	m_timeLimiter = 10;
-	m_enemy = new Enemy(1,1,100);
+
 }
 
 void EnemySpawnComponent::update(float deltaTime)
@@ -16,19 +16,31 @@ void EnemySpawnComponent::update(float deltaTime)
 
 }
 
+/// <summary>
+/// Creats a enemy every second once 
+/// per update 
+/// </summary>
+/// <param name="deltaTime">time deviated based off the CPU</param>
 void EnemySpawnComponent::DupilcateEnemies(float deltaTime)
 {
-
+	// Gets a number between 2 and 30
 	int rng = rand() % 30 + 1 ;
+	//Creats an enemy
 	m_enemy = new Enemy(25 * rng, 5, 400);
-
+	//adds delta time to the timer once per frame 
 	m_timer += deltaTime;
 
+	//check to see if timer ends up beind=g greater then or equal to 
 	if (m_timer >= 1.0f)
 	{
+		// That new enemy to the scene 
 		Engine::getCurrentScene()->addActor(m_enemy);
+		//sets the timer back to 
 		m_timer = 0;
 	}
+	else
+		//Clears un-needed data
+		delete m_enemy;
 
 	
 }
